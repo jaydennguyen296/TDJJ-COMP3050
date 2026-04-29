d403ef0d88315ae87604236c9288599361f473b2
stalebiscuit <161665600+stalebiscuit@users.noreply.github.com>
2026-04-29 08:20:03 +0000

    Changing the server to allow the endpoints for testing.

--- main/server/Server.java
+++ main/server/Server.java
@@ -1,12 +1,14 @@
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
-public class Test {
+public class Server {
 
     public static void main(String[] args) throws Exception {
+        TileMap tileMap = new TileMap();
+        GameState gameState = new GameState(5, 5);
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/info", new MyHandler()); //change myhandler to infohandler
-        server.createContext("/move", new MyHandler()); //change myhandler to movehandler
+        server.createContext("/info", new InfoHandler(tileMap, gameState));
+        server.createContext("/move", new MoveHandler(tileMap, gameState)); //check if the variables are correct
         server.setExecutor(null); // creates a default executor
         server.start();
     }
