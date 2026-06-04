475e082331a1777d46fef21b845143533f6f51d9
tomhann <161672535+tomhann@users.noreply.github.com>
2026-06-04 15:48:28 +1000

    Update security group ingress rules in main.tf to swap HTTP and HTTPS port configurations, setting HTTP to 8000 and HTTPS to 80 for improved access management.

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,16 +29,16 @@ resource "aws_security_group" "Server" {
 
   ingress {
     description = "HTTP"
-    from_port   = 80
-    to_port     = 80
+    from_port   = 8000
+    to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "HTTPS"
-    from_port   = 8000
-    to_port     = 8000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
