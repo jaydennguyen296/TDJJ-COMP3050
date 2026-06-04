e95b9c0ab7d5097a672d745e397912b9b5c1c723
tomhann <161672535+tomhann@users.noreply.github.com>
2026-06-04 16:24:48 +1000

    Add ingress rules for Grafana and Prometheus to the AWS security group in main.tf for enhanced monitoring access.

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,6 +41,23 @@ resource "aws_security_group" "Server" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
